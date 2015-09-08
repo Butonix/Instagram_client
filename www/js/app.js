91ea241ef00f94f559aeda58779eec69ee4615f4
unknown <t3phho00@TYOD-ATK5-E18L.kk.lan>
2015-09-08 16:17:03 +0300

    set up menu bar

--- www/js/app.js
+++ www/js/app.js
@@ -3,17 +3,72 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+//angular.module('starter', ['ionic'])
+var app = angular.module('myApp', ['ionic']);
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-})
+app.run(function($ionicPlatform) {
+    $ionicPlatform.ready(function() {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        if(window.cordova && window.cordova.plugins.Keyboard) {
+          cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        }
+        if(window.StatusBar) {
+          StatusBar.styleDefault();
+        }
+    });
+});
+
+
+app.config(function($stateProvider) {
+    $stateProvider
+
+        .state('app', {
+            url: '/app',
+            absract: true,
+            templateUrl: 'templates/app.html',
+        })
+
+        .state('app.home', {
+            url: '/home',
+            views: {
+              'app-home': {
+                templateUrl: 'templates/home.html',
+                controller: 'HomeCtrl'
+              }
+            }
+        })
+
+        .state('app.search', {
+          url: '/search',
+          views: {
+            'app-search': {
+              templateUrl: 'templates/search.html',
+              controller: 'SearchCtrl'
+            }
+          }
+        })
+
+        .state('app.camera', {
+          url: '/camera',
+          views: {
+            'app-camera': {
+              templateUrl: 'templates/camera.html',
+              controller: 'CameraCtrl'
+            }
+          }
+        })
+
+        .state('app.account', {
+        url: '/account',
+        views: {
+          'app-account': {
+            templateUrl: 'templates/profile.html',
+            controller: 'AccountCtrl'
+          }
+        }
+    });
+
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/app/home');
+});
